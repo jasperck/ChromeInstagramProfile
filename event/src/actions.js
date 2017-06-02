b8652b6756737f6d9ed2916dc8ef8e20809bc2fc
jasperck <jasperc8@gmail.com>
2017-06-02 15:58:30 +0800

    Support loadmore

--- event/src/actions.js
+++ event/src/actions.js
@@ -1,1 +1,2 @@
 export const SET_LIKED = 'SET_LIKED';
+export const UPDATE_LIKED = 'UPDATE_LIKED';
--- event/src/reducers/liked.js
+++ event/src/reducers/liked.js
@@ -1,15 +1,25 @@
-import {SET_LIKED} from '../actions';
+import {
+  SET_LIKED,
+  UPDATE_LIKED
+} from '../actions';
 
 const initialState = {
+  more_available: false,
+  next_max_id: null,
+  auto_load_more_enabled: false,
+  num_results: 0,
   items: []
 };
 
 export const liked = (state = initialState, action) => {
   switch (action.type) {
     case SET_LIKED:
+    case UPDATE_LIKED:
+    console.log(state.items, action.data.items);
       return {
         ...state,
-        ...action.data
+        ...action.data,
+        items: [...state.items, ...action.data.items],
       };
     default:
       return state;
